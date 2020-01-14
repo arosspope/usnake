e51d293b2e3e16b5db487265db1a943e820aa321
arosspope <andrew.pope456@gmail.com>
2020-01-14 15:39:39 +1100

    wip on using correct gpio abstraction

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,45 @@ use cortex_m_rt::entry;
 
 use hal::prelude::*;
 use hal::delay::{self, Delay};
+use hal::gpio::Gpiob;
+use hal::device::GPIOB;
 
-fn initialise() -> (Delay, ITM) {
+fn initialise() -> (Delay, ITM, GPIOB) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = hal::device::Peripherals::take().unwrap();
     
     let mut flash = dp.FLASH.constrain();
-    let rcc = dp.RCC.constrain();
+    let rcc = dp.RCC;
+    rcc.ahbenr.modify(|_, w| w.iopben().set_bit());
     
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
     
-    (Delay::new(cp.SYST, clocks), cp.ITM)
+    let clocks = rcc.constrain().cfgr.freeze(&mut flash.acr);
+    
+    
+    
+    // rcc.ahb.modify(|_, w| w.iopben().set_bit());
+    
+    // PA7 -> Input from motion sensor
+    let gpiob = &dp.GPIOB;
+    gpiob.moder.modify(|_, w| w.moder7().input());
+    gpiob.pupdr.modify(|_, w| w.pupdr7().pull_up());
+    
+    
+    (Delay::new(cp.SYST, clocks), cp.ITM, dp.GPIOB)
 }
 
 
 #[entry]
 fn main() -> ! {
-    let (mut delay, mut itm) = initialise();
+    let (mut delay, mut itm, gpiob) = initialise();
     
-    // Setup serial
-    
-    // Setup GPIO
-
-    // infinite loop; just so we don't leave this stack frame
+    // TODO: Replace ITM with serial
+    let mut counter = 0;
     loop {
-        iprintln!(&mut itm.stim[0], "Hello World!");
+        let detection = gpiob.idr.read().bits();//.idr7().bit_is_set();
+        
+        iprintln!(&mut itm.stim[0], "{}s: {:b}", counter, detection);
+        counter += 1;
         delay.delay_ms(1000_u16);
     }
 }
