530d1763cf8782c450499aced531bdbec3ec8afe
arosspope <andrew.pope456@gmail.com>
2020-01-14 16:27:14 +1100

    working motion detection with hal gpio abstraction

--- src/main.rs
+++ src/main.rs
@@ -9,47 +9,38 @@ use panic_halt;
 use cortex_m::{self, iprint, iprintln, peripheral::ITM};
 use cortex_m_rt::entry;
 
+use hal;
 use hal::prelude::*;
 use hal::delay::{self, Delay};
-use hal::gpio::Gpiob;
 use hal::device::GPIOB;
+use hal::gpio::*;
 
-fn initialise() -> (Delay, ITM, GPIOB) {
+fn initialise() -> (Delay, ITM, hal::gpio::PB7<PullDown, Input>) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = hal::device::Peripherals::take().unwrap();
     
     let mut flash = dp.FLASH.constrain();
-    let rcc = dp.RCC;
-    rcc.ahbenr.modify(|_, w| w.iopben().set_bit());
+    let mut rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
     
+    // PB7 -> Input from motion sensor
+    let gpiob = dp.GPIOB.split(&mut rcc.ahb);
     
-    let clocks = rcc.constrain().cfgr.freeze(&mut flash.acr);
-    
-    
-    
-    // rcc.ahb.modify(|_, w| w.iopben().set_bit());
-    
-    // PA7 -> Input from motion sensor
-    let gpiob = &dp.GPIOB;
-    gpiob.moder.modify(|_, w| w.moder7().input());
-    gpiob.pupdr.modify(|_, w| w.pupdr7().pull_up());
-    
-    
-    (Delay::new(cp.SYST, clocks), cp.ITM, dp.GPIOB)
+    (Delay::new(cp.SYST, clocks), cp.ITM, gpiob.pb7.input().pull_type(hal::gpio::PullDown))
 }
 
 
 #[entry]
 fn main() -> ! {
-    let (mut delay, mut itm, gpiob) = initialise();
+    let (mut delay, mut itm, motion_sensor) = initialise();
+    
+    // delay.delay_ms(60 * 1000_u16);
     
     // TODO: Replace ITM with serial
     let mut counter = 0;
     loop {
-        let detection = gpiob.idr.read().bits();//.idr7().bit_is_set();
-        
-        iprintln!(&mut itm.stim[0], "{}s: {:b}", counter, detection);
+        iprintln!(&mut itm.stim[0], "{}s: {:?}", counter, motion_sensor.is_high().unwrap());
         counter += 1;
-        delay.delay_ms(1000_u16);
+        delay.delay_ms(200_u16);
     }
 }
