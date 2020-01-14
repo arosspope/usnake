795d6f60c06e4640fe8bb0b2be235b34302d6fcf
arosspope <andrew.pope456@gmail.com>
2020-01-14 12:40:59 +1100

    simple ITM logging every second

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,40 @@
 #![no_main]
 #![no_std]
 
-
-#[macro_use]
-use lazy_static;
+// #[macro_use]
+// use lazy_static;
 
 use panic_halt;
+use cortex_m::{self, iprint, iprintln, peripheral::ITM};
+use cortex_m_rt::entry;
 
 use hal::prelude::*;
-// use f3::hal::prelude::*;
-use cortex_m;
-use cortex_m_rt::entry;
+use hal::delay::{self, Delay};
+
+fn initialise() -> (Delay, ITM) {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = hal::device::Peripherals::take().unwrap();
+    
+    let mut flash = dp.FLASH.constrain();
+    let rcc = dp.RCC.constrain();
+    
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    
+    (Delay::new(cp.SYST, clocks), cp.ITM)
+}
+
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut itm) = initialise();
     
     // Setup serial
-    
-    // Setup Delay
-    
-    // Setup ITM
     
     // Setup GPIO
 
     // infinite loop; just so we don't leave this stack frame
-    loop {}
+    loop {
+        iprintln!(&mut itm.stim[0], "Hello World!");
+        delay.delay_ms(1000_u16);
+    }
 }
