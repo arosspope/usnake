fe9b046ec2e7377b996b65b1706285aff0e99847
arosspope <andrew.pope456@gmail.com>
2020-01-19 10:07:13 +1100

    switch hal to use stm32f3xx-hal

--- src/main.rs
+++ src/main.rs
@@ -15,45 +15,40 @@ use cortex_m_rt::entry;
 use hal;
 use hal::prelude::*;
 use hal::delay::{self, Delay};
-use hal::device::{GPIOB, GPIOA};
-use hal::gpio::*;
+use hal::stm32::{GPIOB, GPIOA};
+use hal::gpio::{*, gpioa::*, gpiob::*};
 
 use max7219::*;
 use max7219::connectors::*;
 
 
-type CONNECTOR = PinConnector<PA6<PullNone, Output<PushPull, LowSpeed>>, PA4<PullNone, Output<PushPull, LowSpeed>>, PA5<PullNone, Output<PushPull, LowSpeed>>>;
-// type CONNECTOR = PinConnector<PA6<PullUp, Output<PushPull, LowSpeed>>, PA4<PullUp, Output<PushPull, LowSpeed>>, PA5<PullUp, Output<PushPull, LowSpeed>>>;
+type CONNECTOR = PinConnector<PA6<Output<PushPull>>, PA4<Output<PushPull>>, PA5<Output<PushPull>>>;
 
-// type CONNECTOR = PinConnector<PA6<PullNone, AltFn<AF5, PushPull, LowSpeed>>, PA4<PullNone, AltFn<AF5, PushPull, LowSpeed>>, PA5<PullNone, AltFn<AF5, PushPull, LowSpeed>>>;
 
-fn initialise() -> (Delay, ITM, PB7<PullDown, Input>, MAX7219<CONNECTOR>)
+fn initialise() -> (Delay, ITM, PB7<Input<PullDown>>, MAX7219<CONNECTOR>)
 {
     let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = hal::device::Peripherals::take().unwrap();
+    let dp = hal::stm32::Peripherals::take().unwrap();
 
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     // PB7 -> Input from motion sensor
-    let gpiob           = dp.GPIOB.split(&mut rcc.ahb);
-    let motion_sensor   = gpiob.pb7.input().pull_type(hal::gpio::PullDown);
+    let mut gpiob       = dp.GPIOB.split(&mut rcc.ahb);
+    let motion_sensor   = gpiob.pb7.into_pull_down_input(&mut gpiob.moder, &mut gpiob.pupdr);
 
     // SPI1 for display (PA4=NSS, PA5=SCK, PA6=MISO, PA7=MOSI)
-    let gpioa   = dp.GPIOA.split(&mut rcc.ahb);
-    // let data    = gpioa.pa6.alternating(hal::gpio::AF5);
-    // let sck     = gpioa.pa5.alternating(hal::gpio::AF5);
-    // let cs      = gpioa.pa4.alternating(hal::gpio::AF5);
-    let data       = gpioa.pa6.output();
-    let sck        = gpioa.pa5.output();
-    let cs         = gpioa.pa4.output();
-    let display = MAX7219::from_pins(1, data, cs, sck).unwrap();
+    let mut gpioa   = dp.GPIOA.split(&mut rcc.ahb);
+    let data        = gpioa.pa6.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let sck         = gpioa.pa5.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let cs          = gpioa.pa4.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let display     = MAX7219::from_pins(1, data, cs, sck).unwrap();
 
     (Delay::new(cp.SYST, clocks), cp.ITM, motion_sensor, display)
 }
 
-fn wait_for_motion(sensor: &PB7<PullDown, Input>) {
+fn _wait_for_motion(sensor: &PB7<Input<PullDown>>) {
     while sensor.is_low().unwrap() {}
 }
 
@@ -61,23 +56,23 @@ fn wait_for_motion(sensor: &PB7<PullDown, Input>) {
 #[entry]
 fn main() -> ! {
     // let (mut delay, mut itm, motion_sensor, _data, _cs, _sck) = initialise();
-    let (mut delay, mut itm, motion_sensor, mut display) = initialise();
+    let (mut delay, mut itm, _motion_sensor, mut display) = initialise();
 
     // display.power_off().unwrap();
     // wait_for_motion(&motion_sensor);
     // delay.delay_ms(1000_u16);
-    
+
     // make sure to wake the display up
     display.power_on().unwrap();
     // wait_for_motion(&motion_sensor);
     // delay.delay_ms(1000_u16);
-    
-    
+
+
     // write given octet of ASCII characters with dots specified by 3rd param bits
     // display.write_str(0, b"a a a a ", 0b1111111).unwrap();
-    
-    
-    
+
+
+
     // wait_for_motion(&motion_sensor);
     // delay.delay_ms(1000_u16);
     display.test(0, false).unwrap();
@@ -92,7 +87,7 @@ fn main() -> ! {
     loop {
         // iprintln!(&mut itm.stim[0], "[{}] motion detected - {:?}", counter, motion_sensor.is_high().unwrap());
         // display.clear_display(0).unwrap();
-        
+
         let matrix: [u8; 8] = [
             counter % 255,
             (counter + 1) % 254,
@@ -103,13 +98,13 @@ fn main() -> ! {
             (counter + 6) % 254,
             (counter + 7) % 254,
         ];
-        
+
         match display.write_raw(0, &matrix) {
             Err(_) => iprintln!(&mut itm.stim[0], "[ERROR] Refreshing display failed"),
             _ => (),
         }
         // display.write_str(0, b"........", 0b11010101).unwrap();
-        
+
         counter += 1;
         delay.delay_ms(150_u16);
     }
