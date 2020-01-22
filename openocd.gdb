46637e1705acf2c28e70f85b1f6ceaa957ae9e56
arosspope <andrew.pope456@gmail.com>
2020-01-22 12:10:52 +1100

    working snake game with rtfm

--- openocd.gdb
+++ openocd.gdb
@@ -4,9 +4,10 @@ set print pretty on
 set backtrace limit 32
 
 # Detect unhandled exceptions, hard faults and panics
-break DefaultHandler
-break HardFault
-# break rust_begin_unwind
+# NB: In the currently configured crate, panic-itm
+# is used to print panic messages out through ITM
+# break DefaultHandler
+# break HardFault
 
 # Set ITM
 monitor tpiu config internal itm.txt uart off 8000000
@@ -15,5 +16,5 @@ monitor itm port 0 on
 load
 
 layout src
-break idle
+break idlescreen
 continue
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@
 #![no_std]
 
 
+use rtfm::cyccnt::Duration;
+use rtfm::cyccnt::{Instant, U32Ext};
 use panic_itm;
 use cortex_m::{self, iprintln, itm, peripheral::ITM};
 use cortex_m_rt::{entry, exception, ExceptionFrame};
 use hal::{
     prelude::*,
     delay::Delay,
-    time::MonoTimer,
+    time::{Hertz, MonoTimer},
     adc::*,
     stm32::{Peripherals, interrupt, Interrupt}
 };
@@ -19,119 +21,6 @@ use hal::{
 use max7219::*;
 use usnake::{game::*, joystick::*};
 
-// fn initialise() -> (Delay, ITM, Game)
-// {
-//     let cp = cortex_m::Peripherals::take().unwrap();
-//     let mut dp = hal::stm32::Peripherals::take().unwrap();
-//
-//     let mut flash = dp.FLASH.constrain();
-//     let mut rcc = dp.RCC.constrain();
-//     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-//
-//     // Display pins
-//     let mut gpiob   = dp.GPIOB.split(&mut rcc.ahb);
-//     let data        = gpiob.pb8.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
-//     let cs          = gpiob.pb9.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
-//     let sck         = gpiob.pb10.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
-//     let display     = MAX7219::from_pins(1, data, cs, sck).expect("Unable to initialise display");
-//
-//     let mut gpioa   = dp.GPIOA.split(&mut rcc.ahb);
-//     let joystick    = Joystick::from_pins(
-//         Adc::adc1(dp.ADC1, &mut dp.ADC1_2, &mut rcc.ahb, clocks),
-//         Adc::adc2(dp.ADC2, &mut dp.ADC1_2, &mut rcc.ahb, clocks),
-//         gpioa.pa0.into_analog(&mut gpioa.moder, &mut gpioa.pupdr),
-//         gpioa.pa4.into_analog(&mut gpioa.moder, &mut gpioa.pupdr),
-//         gpioa.pa2.into_pull_up_input(&mut gpioa.moder, &mut gpioa.pupdr)
-//     ).expect("Unable to initialise joystick class");
-//
-//     let controller = Game::new(
-//         MonoTimer::new(cp.DWT, clocks).now(),
-//         display,
-//         joystick
-//     );
-//
-//     (Delay::new(cp.SYST, clocks), cp.ITM, controller)
-// }
-//
-// fn screensaver(count: u8) -> [u8; 8] {
-//     let display: [u8; 8] = [
-//         count,
-//         ((count as u16 + 1) % 256) as u8,
-//         ((count as u16 + 2) % 256) as u8,
-//         ((count as u16 + 3) % 256) as u8,
-//         ((count as u16 + 4) % 256) as u8,
-//         ((count as u16 + 5) % 256) as u8,
-//         ((count as u16 + 6) % 256) as u8,
-//         ((count as u16 + 7) % 256) as u8,
-//     ];
-//
-//     display
-// }
-//
-//
-// fn wait_for_user(delay: &mut Delay, controller: &mut Game) {
-//     controller.display.power_on().expect("error with display");
-//
-//     let mut count = 0;
-//     loop {
-//         // While waiting for user input display screensaver
-//         if controller.joystick.is_pressed().expect("error with joystick"){
-//             break;
-//         }
-//
-//         controller.display.write_raw(0, &screensaver(count)).expect("could not write to display");
-//         count = (count + 1) % 255;
-//         delay.delay_ms(100_u16);
-//     }
-// }
-//
-// fn usnake(delay: &mut Delay, controller: &mut Game) -> usize {
-//     loop {
-//         if let Some(score) = controller.tick() {
-//             // Game complete!
-//             for _ in 0..7 {
-//                 controller.display.power_off().expect("error with display");
-//                 delay.delay_ms(500_u16);
-//                 controller.display.power_on().expect("error with display");
-//                 delay.delay_ms(500_u16);
-//             }
-//             controller.reset();
-//             return score
-//         };
-//
-//         delay.delay_ms(200_u16);
-//     }
-// }
-//
-// #[entry]
-// fn main() -> ! {
-//     let (mut delay, mut itm, mut controller) = initialise();
-//
-//     // display.power_off().unwrap();
-//     // wait_for_motion(&motion_sensor);
-//     // delay.delay_ms(1000_u16);
-//
-//     // make sure to wake the display up
-//     // gameworld.display.power_on().unwrap();
-//     // set display intensity lower
-//     // display.set_intensity(0, 0x1).unwrap();
-//
-//     loop {
-//         iprintln!(&mut itm.stim[0], "waiting for user input....");
-//         wait_for_user(&mut delay, &mut controller);
-//
-//         iprintln!(&mut itm.stim[0], "game start...");
-//         let score = usnake(&mut delay, &mut controller);
-//         iprintln!(&mut itm.stim[0], "game end - final score: {}", score);
-//     }
-//
-//
-//
-//     // TODO:
-//     //  [] Use RTFM to orchestrate the game
-//     //      - https://rtfm.rs/0.5/book/en/preface.html
-//     //      - https://github.com/rnestler/hello-rtfm-rs
-// }
 use core::fmt::{self, Write};
 
 /// Macro for sending a formatted string through an ITM channel, with a newline.
@@ -160,26 +49,29 @@ macro_rules! logprintln {
     };
 }
 
-
-
 use rtfm::{app, Exclusive, Mutex};
 
-#[app(device = hal::stm32, peripherals = true)]
+#[app(device = hal::stm32, peripherals = true, monotonic = rtfm::cyccnt::CYCCNT)]
 const SNAKE: () = {
     // Late resources
     struct Resources {
         logger: ITM,
-        controller: Game
+        controller: Game,
+        sysclk_hz: Hertz
     }
 
-    #[init(spawn = [foo])]
+    #[init(spawn = [idlescreen])]
     fn init(context: init::Context) -> init::LateResources {
-        let mut core        : cortex_m::Peripherals     = context.core;         // Cortex-M peripherals
+        let mut core    : rtfm::Peripherals         = context.core;         // Cortex-M peripherals
         let mut device  : hal::stm32::Peripherals   = context.device;   // Device specific peripherals
 
         let mut flash   = device.FLASH.constrain();
         let mut rcc     = device.RCC.constrain();
         let clocks      = rcc.cfgr.freeze(&mut flash.acr);
+
+        // Initialize (enable) the monotonic timer (CYCCNT)
+        core.DCB.enable_trace();
+        core.DWT.enable_cycle_counter();
 
         // Setup for the MAX7219 display
         let mut gpiob   = device.GPIOB.split(&mut rcc.ahb);
@@ -205,48 +97,67 @@ const SNAKE: () = {
             joystick
         );
 
-        // Spawn foo thread
-        context.spawn.foo().unwrap();
-        iprintln!(&mut core.ITM.stim[0], "... finished initialisation ...");
+        iprintln!(&mut core.ITM.stim[0], "... app start ...");
+        context.spawn.idlescreen().expect("something went wrong whilst spawning the 'idlescreen' task");
 
-        init::LateResources { logger: core.ITM, controller: controller }
+        init::LateResources { logger: core.ITM, controller: controller, sysclk_hz: clocks.sysclk() }
     }
 
-    #[idle(resources = [logger])]
-    fn idle(mut context: idle::Context) -> ! {
-        // The idle task gets directly executed after init and should not return
-        // RTFM will substitute its own handler if this is not declared
-        logprintln!(&mut context.resources.logger, "... hit the idle task ...");
 
-        loop {}
+    #[task(spawn = [gametick], resources = [logger, controller, sysclk_hz], schedule = [idlescreen])]
+    fn idlescreen(context: idlescreen::Context) {
+        static mut SCREEN: [u8; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
+        for i in 0..SCREEN.len() {
+            SCREEN[i] = ((SCREEN[i] + 1) as u16 % 256) as u8 % 255;
+        }
+
+        context.resources.controller.display.write_raw(0, &SCREEN).expect("unable to refresh screen");
+
+        if context.resources.controller.joystick.is_pressed().expect("error with joystick") {
+            logprintln!(Exclusive(context.resources.logger), "... game start ...");
+            // Start game...
+            context.spawn.gametick().expect("something went wrong whilst spawning the 'gametick' task");
+        } else {
+            // Every 150ms idlescreen is refreshed
+            let delay = ((150e-3 as f32 * context.resources.sysclk_hz.0 as f32) as u32).cycles();
+            context.schedule.idlescreen(context.scheduled + delay).expect("unable to schedule the idlescreen task");
+        }
     }
 
-    #[task(spawn = [bar, baz], resources = [logger])]
-    fn foo(mut context: foo::Context) {
-        logprintln!(&mut context.resources.logger, "foo - start");
-        //
-        // // spawns `bar` onto the task scheduler
-        // // `foo` and `bar` have the same priority so `bar` will not run until
-        // // after `foo` terminates
-        context.spawn.bar().unwrap();
-        //
-        logprintln!(&mut context.resources.logger, "foo - middle");
-        //
-        // // spawns `baz` onto the task scheduler
-        // // `baz` has higher priority than `foo` so it immediately preempts `foo`
-        context.spawn.baz().unwrap();
-        //
-        logprintln!(&mut context.resources.logger, "foo - end");
+    #[task(spawn = [gameend], resources = [logger, controller, sysclk_hz], schedule = [gametick])]
+    fn gametick(context: gametick::Context) {
+        if let Some(score) = context.resources.controller.tick() {
+            context.spawn.gameend().expect("something went wrong whilst spawning the 'gameend' task");
+            logprintln!(Exclusive(context.resources.logger), "... game end - final score: {} ...", score);
+        } else {
+            let delay = ((250e-3 as f32 * context.resources.sysclk_hz.0 as f32) as u32).cycles(); // Every 250ms the game 'ticks'
+            context.schedule.gametick(context.scheduled + delay).expect("unable to schedule the gametick task");
+        }
     }
 
-    #[task(resources = [logger])]
-    fn bar(mut context: bar::Context) {
-        logprintln!(&mut context.resources.logger, "bar");
-    }
+    #[task(spawn = [idlescreen], resources = [controller, sysclk_hz], schedule = [gameend])]
+    fn gameend(context: gameend::Context) {
+        static mut SCREEN_ON: bool = true;
+        static mut FLASH_SCREEN_COUNT: u8 = 0;
 
-    #[task(priority = 2, resources = [logger])]
-    fn baz(context: baz::Context) {
-        logprintln!(Exclusive(context.resources.logger), "baz");
+        if *FLASH_SCREEN_COUNT < 7 {
+            match *SCREEN_ON {
+                true => context.resources.controller.display.power_off().expect("unable to toggle display"),
+                false => context.resources.controller.display.power_on().expect("unable to toggle display"),
+            }
+
+
+            let delay = ((500e-3 as f32 * context.resources.sysclk_hz.0 as f32) as u32).cycles(); // Every 250ms the game 'ticks'
+            context.schedule.gameend(context.scheduled + delay).expect("unable to schedule the gametick task");
+            *FLASH_SCREEN_COUNT += 1;
+            *SCREEN_ON = !*SCREEN_ON;
+        } else {
+            *SCREEN_ON = true;
+            *FLASH_SCREEN_COUNT = 0;
+            context.resources.controller.reset();
+            context.resources.controller.display.power_on().expect("unable to toggle display");
+            context.spawn.idlescreen().expect("something went wrong whilst spawning the 'idlescreen' task");
+        }
     }
 
     // Interrupt handlers used to dispatch software tasks
@@ -255,11 +166,11 @@ const SNAKE: () = {
     // some of these free interrupts will be used to dispatch the software tasks. An advantage of software
     // tasks over hardware tasks is that many tasks can be mapped to a single interrupt handler.
     extern "C" {
-        fn DMA1_CH1();
-        fn DMA1_CH2();
+        fn DMA1_CH1(); // Not using the DMA1_CH1 interrupt
     }
 
 };
+
 
 #[exception]
 fn HardFault(ef: &ExceptionFrame) -> ! {
